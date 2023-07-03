c25f4bc1c79948b406b0d58a92f53f8cbab4055d
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2023-07-03 16:50:21 +0200

    Run cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -20,15 +20,14 @@
 //! ```
 
 #![no_std]
-use embedded_hal::i2c::{I2c, ErrorType};
-use embedded_hal::delay::DelayUs;
 use bitfield::bitfield;
+use embedded_hal::delay::DelayUs;
+use embedded_hal::i2c::{ErrorType, I2c};
 
 pub struct Driver<I2C> {
     i2c: I2C,
     integration_time: IntegrationTimes,
-    gain: Gain
-
+    gain: Gain,
 }
 
 impl<E> From<E> for Error<E> {
@@ -39,13 +38,13 @@ impl<E> From<E> for Error<E> {
 
 impl<I2C> Driver<I2C>
 where
-    I2C: I2c + ErrorType
+    I2C: I2c + ErrorType,
 {
     pub fn new(i2c: I2C) -> Result<Driver<I2C>, Error<I2C::Error>> {
         let mut driver = Driver {
             i2c,
             integration_time: IntegrationTimes::_200MS,
-            gain: Gain::LOW
+            gain: Gain::LOW,
         };
         let id = driver.get_id()?;
         if id != chip::ID {
@@ -54,12 +53,15 @@ where
         Ok(driver)
     }
 
-    pub fn new_define_integration(i2c: I2C, integration_time: IntegrationTimes, gain: Gain) -> Result<Driver<I2C>, Error<I2C::Error>> {
+    pub fn new_define_integration(
+        i2c: I2C,
+        integration_time: IntegrationTimes,
+        gain: Gain,
+    ) -> Result<Driver<I2C>, Error<I2C::Error>> {
         let mut driver = Driver {
             i2c,
             integration_time,
-            gain
-
+            gain,
         };
         let id = driver.get_id()?;
         if id != chip::ID {
@@ -70,74 +72,122 @@ where
 
     fn get_id(&mut self) -> Result<u8, Error<I2C::Error>> {
         let mut buffer = [0u8; 1];
-        self.i2c.write_read(chip::I2C, &[chip::COMMAND_BIT | chip::ID_ADDR], &mut buffer)?;
+        self.i2c
+            .write_read(chip::I2C, &[chip::COMMAND_BIT | chip::ID_ADDR], &mut buffer)?;
         Ok(buffer[0])
     }
 
     pub fn set_gain(&mut self, gain: Option<Gain>) -> Result<(), Error<I2C::Error>> {
         if let Some(gain) = gain {
-            self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::CONTROL, self.integration_time as u8 | gain as u8])?;
+            self.i2c.write(
+                0x29,
+                &[
+                    chip::COMMAND_BIT | chip::CONTROL,
+                    self.integration_time as u8 | gain as u8,
+                ],
+            )?;
         } else {
-            self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::CONTROL, self.integration_time as u8 | self.gain as u8])?;
+            self.i2c.write(
+                0x29,
+                &[
+                    chip::COMMAND_BIT | chip::CONTROL,
+                    self.integration_time as u8 | self.gain as u8,
+                ],
+            )?;
         }
         Ok(())
     }
 
-    pub fn set_timing(&mut self, integration_time: Option<IntegrationTimes>) -> Result<(), Error<I2C::Error>> {
+    pub fn set_timing(
+        &mut self,
+        integration_time: Option<IntegrationTimes>,
+    ) -> Result<(), Error<I2C::Error>> {
         if let Some(integration_time) = integration_time {
-            self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::CONTROL, integration_time as u8| self.gain as u8])?;
+            self.i2c.write(
+                0x29,
+                &[
+                    chip::COMMAND_BIT | chip::CONTROL,
+                    integration_time as u8 | self.gain as u8,
+                ],
+            )?;
         } else {
-            self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::CONTROL, self.integration_time as u8 | self.gain as u8])?;
+            self.i2c.write(
+                0x29,
+                &[
+                    chip::COMMAND_BIT | chip::CONTROL,
+                    self.integration_time as u8 | self.gain as u8,
+                ],
+            )?;
         }
         Ok(())
     }
 
     pub fn disable(&mut self) -> Result<(), Error<I2C::Error>> {
-        self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::ENABLE, chip::ENABLE_POWEROFF])?;
+        self.i2c.write(
+            0x29,
+            &[chip::COMMAND_BIT | chip::ENABLE, chip::ENABLE_POWEROFF],
+        )?;
         Ok(())
     }
 
     pub fn enable(&mut self) -> Result<(), Error<I2C::Error>> {
-        self.i2c.write(0x29, &[chip::COMMAND_BIT | chip::ENABLE,
-            chip::ENABLE_POWERON | chip::ENABLE_AEN | chip::ENABLE_AIEN | chip::ENABLE_NPIEN])?;
+        self.i2c.write(
+            0x29,
+            &[
+                chip::COMMAND_BIT | chip::ENABLE,
+                chip::ENABLE_POWERON | chip::ENABLE_AEN | chip::ENABLE_AIEN | chip::ENABLE_NPIEN,
+            ],
+        )?;
         Ok(())
     }
 
     pub fn get_enable(&mut self) -> Result<Enable, Error<I2C::Error>> {
         let mut status = [0u8; 1];
-        self.i2c.write_read(chip::I2C, &[chip::COMMAND_BIT | chip::ENABLE], &mut status)?;
+        self.i2c
+            .write_read(chip::I2C, &[chip::COMMAND_BIT | chip::ENABLE], &mut status)?;
         Ok(Enable(status[0]))
     }
 
     pub fn get_status(&mut self) -> Result<Status, Error<I2C::Error>> {
         let mut status = [0u8; 1];
-        self.i2c.write_read(chip::I2C, &[chip::COMMAND_BIT | chip::STATUS], &mut status)?;
+        self.i2c
+            .write_read(chip::I2C, &[chip::COMMAND_BIT | chip::STATUS], &mut status)?;
         Ok(Status(status[0]))
     }
 
-
-    pub fn get_channel_data<D: DelayUs>(&mut self, delay: &mut D) -> Result<(u16, u16), Error<I2C::Error>> {
+    pub fn get_channel_data<D: DelayUs>(
+        &mut self,
+        delay: &mut D,
+    ) -> Result<(u16, u16), Error<I2C::Error>> {
         delay.delay_ms(120);
         let mut buffer_1 = [0u8; 2];
         let mut buffer_2 = [0u8; 2];
-        self.i2c.write_read(chip::I2C, &[chip::COMMAND_BIT | chip::CHAN0_LOW], &mut buffer_1)?;
-        self.i2c.write_read(chip::I2C, &[chip::COMMAND_BIT | chip::CHAN1_LOW], &mut buffer_2)?;
+        self.i2c.write_read(
+            chip::I2C,
+            &[chip::COMMAND_BIT | chip::CHAN0_LOW],
+            &mut buffer_1,
+        )?;
+        self.i2c.write_read(
+            chip::I2C,
+            &[chip::COMMAND_BIT | chip::CHAN1_LOW],
+            &mut buffer_2,
+        )?;
         let channel_0 = ((buffer_1[0] as u16) << 8) | buffer_1[1] as u16;
         let channel_1 = ((buffer_2[0] as u16) << 8) | buffer_2[1] as u16;
         Ok((channel_0, channel_1))
     }
 
-    pub fn get_luminosity<D: DelayUs>(&mut self, mode: Mode, delay: &mut D) -> Result<u16, Error<I2C::Error>> {
+    pub fn get_luminosity<D: DelayUs>(
+        &mut self,
+        mode: Mode,
+        delay: &mut D,
+    ) -> Result<u16, Error<I2C::Error>> {
         let (channel_0, channel_1) = self.get_channel_data(delay)?;
-        let full_luminosity: u32 = ((channel_1 as u32) << 16 ) | channel_0 as u32;
+        let full_luminosity: u32 = ((channel_1 as u32) << 16) | channel_0 as u32;
 
         match mode {
-            Mode::FullSpectrum => {
-                Ok(( full_luminosity & 0xFFFF ) as u16)
-            }
-            Mode::Infrared => {
-                Ok((full_luminosity >> 16) as u16)
-            }
+            Mode::FullSpectrum => Ok((full_luminosity & 0xFFFF) as u16),
+            Mode::Infrared => Ok((full_luminosity >> 16) as u16),
             Mode::Visible => {
                 let infrared_and_visible = full_luminosity & 0xFFFF;
                 let infrared = full_luminosity >> 16;
@@ -152,24 +202,12 @@ where
 
     fn get_integration_in_ms(&self) -> f32 {
         match self.integration_time {
-            IntegrationTimes::_100MS => {
-                100.
-            }
-            IntegrationTimes::_200MS => {
-                200.
-            }
-            IntegrationTimes::_300MS => {
-                300.
-            }
-            IntegrationTimes::_400MS => {
-                400.
-            }
-            IntegrationTimes::_500MS => {
-                500.
-            }
-            IntegrationTimes::_600MS => {
-                600.
-            }
+            IntegrationTimes::_100MS => 100.,
+            IntegrationTimes::_200MS => 200.,
+            IntegrationTimes::_300MS => 300.,
+            IntegrationTimes::_400MS => 400.,
+            IntegrationTimes::_500MS => 500.,
+            IntegrationTimes::_600MS => 600.,
         }
     }
 
@@ -191,7 +229,7 @@ where
         let a_time = self.get_integration_in_ms();
         let a_gain = self.get_gain_multiplier();
         let cpl = (a_time * a_gain) / 408.0;
-        let lux = ((ch_0 as f32 - ch_1 as f32)) * (1.0 - (ch_1 as f32  / ch_0 as f32)) / cpl;
+        let lux = (ch_0 as f32 - ch_1 as f32) * (1.0 - (ch_1 as f32 / ch_0 as f32)) / cpl;
 
         Ok(lux)
     }
@@ -205,20 +243,20 @@ pub enum Mode {
 
 #[derive(Clone, Copy)]
 pub enum IntegrationTimes {
-    _100MS = 0x00, // 100 
+    _100MS = 0x00, // 100
     _200MS = 0x01, // 200 millis
     _300MS = 0x02, // 300 millis
     _400MS = 0x03, // 400 millis
     _500MS = 0x04, // 500 millis
-    _600MS = 0x05 // 600 millis
+    _600MS = 0x05, // 600 millis
 }
 
 #[derive(Clone, Copy)]
 pub enum Gain {
-  LOW = 0x00,  // low gain (1x)
-  MED = 0x10,  // medium gain (25x)
-  HIGH = 0x20, // medium gain (428x)
-  MAX= 0x30  // max gain (9876x)
+    LOW = 0x00,  // low gain (1x)
+    MED = 0x10,  // medium gain (25x)
+    HIGH = 0x20, // medium gain (428x)
+    MAX = 0x30,  // max gain (9876x)
 }
 
 mod chip {
@@ -228,10 +266,10 @@ mod chip {
     pub const COMMAND_BIT: u8 = 0xA0;
     pub const ENABLE_POWERON: u8 = 0x01;
     pub const ENABLE_AEN: u8 = 0x02;
-    pub const ENABLE_AIEN: u8 =  0x10;
+    pub const ENABLE_AIEN: u8 = 0x10;
     pub const ENABLE_POWEROFF: u8 = 0x00;
     pub const ENABLE_NPIEN: u8 = 0x80;
-    pub const CHAN0_LOW: u8 =  0x14;
+    pub const CHAN0_LOW: u8 = 0x14;
     pub const CHAN1_LOW: u8 = 0x16;
     pub const ENABLE: u8 = 0x00;
     pub const CONTROL: u8 = 0x01;
@@ -251,7 +289,7 @@ pub enum Error<I2cError> {
     I2cError(I2cError),
     IdMismatch(u8),
     SignalOverflow(),
-    InfraredOverflow()
+    InfraredOverflow(),
 }
 
 enum PersitFilter {
@@ -272,7 +310,7 @@ enum PersitFilter {
     TSL2591_PERSIST_45 = 0x0C,    // 45 consecutive values out of range
     TSL2591_PERSIST_50 = 0x0D,    // 50 consecutive values out of range
     TSL2591_PERSIST_55 = 0x0E,    // 55 consecutive values out of range
-    TSL2591_PERSIST_60 = 0x0F     // 60 consecutive values out of range
+    TSL2591_PERSIST_60 = 0x0F,    // 60 consecutive values out of range
 }
 
 bitfield! {
